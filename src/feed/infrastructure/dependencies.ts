26735481a70aa7bd9507c46369daff7617e1aabf
swaldlyons <oswaldo.leon.c@gmail.com>
2023-05-13 20:45:17 +0200

    Arreglo en la arquitectura

--- src/feed/infrastructure/dependencies.ts
+++ src/feed/infrastructure/dependencies.ts
@@ -1,6 +1,6 @@
 import { getDailyNews } from '../../shared/infrastructure/dependencies';
-import { FeedRepository } from './repository/feed.repository';
+import { Feed } from '../application/feed';
 import { FeedController } from './rest/feed.controller';
 
-export const feedRepository = new FeedRepository();
+export const feedRepository = new Feed();
 export const feedController = new FeedController(getDailyNews, feedRepository);
