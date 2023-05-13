aa4119e40fc58fdb8e751f5395d72b449e8e36f8
swaldlyons <oswaldo.leon.c@gmail.com>
2023-05-13 12:30:58 +0200

    Primeras 5 noticias del día de elmundo.es

--- src/feed/infrastructure/rest/feed.router.ts
+++ src/feed/infrastructure/rest/feed.router.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 import express, { Request, Response, Router } from 'express';
 import { body } from 'express-validator';
 
@@ -5,8 +7,28 @@ import { validateFields } from '../../../shared/infrastructure/validation/valida
 
 const feedRouter: Router = express.Router();
 
-feedRouter.get('/', (req: Request, res: Response) => {
-  res.json({ hello: 'world!' });
+feedRouter.get('/', async (req: Request, res: Response) => {
+  const news = [];
+  const url = 'https://www.elmundo.es/';
+  const { data, status } = await axios.get(url);
+
+  if (status === 200) {
+    const $: cheerio.CheerioAPI = cheerio.load(data);
+    const principal: cheerio.Cheerio<cheerio.Element> = $(
+      '.ue-c-cover-content__main .ue-c-cover-content__headline-group',
+    );
+
+    principal.each((i: number, e: cheerio.Element) => {
+      news.push({
+        title: $(e).find('span').text(),
+        resume: $(e).find('h2').text(),
+        link: $(e).find('a').attr('href'),
+      });
+      if (i === 4) return false;
+    });
+  }
+
+  res.json(news);
 });
 
 feedRouter.post(
